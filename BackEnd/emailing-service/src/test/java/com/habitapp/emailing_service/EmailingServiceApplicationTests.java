bd57afc288e0a710f7d01981868dee44fb4424db
Yassine-Chalati <yassine.chalati.07@gmail.com>
2024-11-10 01:03:20 +0100

    create backend micorservcices

---
+++ BackEnd/emailing-service/src/test/java/com/habitapp/emailing_service/EmailingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitapp.emailing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
