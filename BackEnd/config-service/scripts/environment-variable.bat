c54fbf1323a7b74ca951764add22032eb03df508
Yassine-Chalati <yassine.chalati.07@gmail.com>
2024-11-25 00:14:07 +0100

    error token

--- BackEnd/config-service/scripts/environment-variable.bat
+++ BackEnd/config-service/scripts/environment-variable.bat
@@ -1,4 +1,4 @@
 setx GIT_URI "https://github.com/Yassine-Chalati/HabitAppConfigurationRepository.git" /M
 setx USERNAME_GIT "Yassine-Chalati" /M
-setx PASSWORD_GIT "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" /M
+setx PASSWORD_GIT "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" /M
 setx TRY_MASTER_BRANCH "false" /M
--- BackEnd/config-service/scripts/environment-variable.sh
+++ BackEnd/config-service/scripts/environment-variable.sh
@@ -1,4 +1,4 @@
 export GIT_URI="https://github.com/Yassine-Chalati/HabitAppConfigurationRepository.git"
 export USERNAME_GIT="Yassine-Chalati"
-export PASSWORD_GIT="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"
+export PASSWORD_GIT="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"
 export TRY_MASTER_BRANCH="false"
